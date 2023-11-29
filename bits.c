caf66591e1c1eeeeac5888b1afc0158d10b6c123
Matias Salem <matiassalem17@gmail.com>
2023-11-28 21:26:16 -0500

    Update bits.c

--- bits.c
+++ bits.c
@@ -461,21 +461,25 @@ unsigned floatScale64(unsigned uf) {
         // Denormalized number: scale the fraction
         fraction <<= 6;
 
-        // Normalize if necessary
-        while ((fraction & 0x00800000) == 0 && fraction != 0) {
-            fraction <<= 1;
-            exponent -= 0x00800000; // Decrease exponent (increase the power of 2)
+        if (fraction & 0x00800000) {
+            // Fraction has overflowed into the exponent field
+            exponent = 0x00800000; // Adjust exponent to 1
+            fraction &= 0x007FFFFF; // Remove the overflowed bit
         }
 
-        if ((exponent & 0x7F800000) == 0) {
-            // Still a denormalized number
-            return sign | (fraction & 0x007FFFFF);
-        } else {
-            // Now a normalized number
-            fraction >>= 1; // Shift right to adjust for leading 1
-            exponent += 0x01000000; // Correct the exponent
-            return sign | exponent | (fraction & 0x007FFFFF);
+        // Check for any further normalization needed
+        while ((fraction & 0x00800000) == 0 && fraction != 0) {
+            fraction <<= 1;
+            exponent -= 0x00800000; // Adjust exponent
         }
+
+        if (exponent == 0) {
+            // Still a denormalized number
+            return sign | fraction;
+        }
+
+        // Normalized number
+        return sign | (exponent + 0x00800000) | (fraction & 0x007FFFFF);
     }
 
     // Normalized number: increment the exponent
@@ -487,6 +491,7 @@ unsigned floatScale64(unsigned uf) {
 
     return sign | exponent | fraction;
 }
+
 
 
 
