a7338debd02c62d896b96be530996c06e196d043
Matias Salem <matiassalem17@gmail.com>
2023-11-28 20:31:26 -0500

    Update bits.c

--- bits.c
+++ bits.c
@@ -448,37 +448,37 @@ unsigned floatInt2Float(int x) {
  *   Rating: 4
  */
 unsigned floatScale64(unsigned uf) {
+
+    // begin by taking the sign, the exponent, and the fraction
     unsigned sign = uf & 0x80000000;
     unsigned exp = uf & 0x7F800000;
     unsigned frac = uf & 0x007FFFFF;
 
-    // Check for NaN or infinity
+    // return number if exponent is NaN
     if (exp == 0x7F800000) return uf;
 
-    // Handling denormalized numbers
+    // in case uf is Denormalized
     if (exp == 0) {
-        frac <<= 6; // Multiply fraction by 64
 
-        // Check if it gets normalized
+        // multiply by 64, which is 2^6
+        frac = frac << 6;
+
+        // if the number is normalized by the scaling
         if (frac & 0x00800000) {
             exp = 0x00800000; // Set to the smallest exponent for normalized numbers
-            frac &= 0x007FFFFF; // Remove the leading 1
+            frac &= 0x007FFFFF;
         }
         return sign | exp | frac;
     }
 
-    // Handling normalized numbers
-    exp = exp + (6 << 23); // Increase exponent by 6
+    // add 6 to the exponent
+    exp = exp + (6 << 23);
 
-    // Check for exponent overflow
-    if (exp >= 0x7F800000) {
-        // Return infinity with the same sign
-        return sign | 0x7F800000;
-    }
+    // if there is an overflow
+    if (exp >= 0x7F800000) return sign | 0x7F800000;
 
     return sign | exp | frac;
 }
-
 /*
  * floatNegate - Return bit-level equivalent of expression -f for
  *   floating point argument f.
